9e4e4264b7ff2a2a30bee57e3d981e1658e6fde1
shyjuissac <shyjuissacalex@gmail.com>
2021-07-01 17:56:53 +0530

    all new file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
+import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import Menu2 from './components/Menu2';
+import MoreContent from './components/MoreContent';
+import Footer from './components/Footer';
+import { BrowserRouter } from 'react-router-dom';
+import { Route } from 'workbox-routing';
 
 function App() {
   return (
     <div className="App">
-<Header/>
-<Menu2/>
+        <Header/>
+        <Menu2/>
+        <MoreContent/>
+        <Footer/>
     </div>
   );
 }
