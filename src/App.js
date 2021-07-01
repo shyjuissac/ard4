6d92e646c0b56fe868775a51e6656d32e2f17c89
shyjuissac <shyjuissacalex@gmail.com>
2021-07-01 18:04:09 +0530

    some delet

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
-import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import Menu2 from './components/Menu2';
-import MoreContent from './components/MoreContent';
-import Footer from './components/Footer';
-import { BrowserRouter } from 'react-router-dom';
-import { Route } from 'workbox-routing';
 
 function App() {
   return (
     <div className="App">
-        <Header/>
-        <Menu2/>
-        <MoreContent/>
-        <Footer/>
+<Header/>
+<Menu2/>
     </div>
   );
 }
