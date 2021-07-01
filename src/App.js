f31f9b10b6769d72620bf7f83854cb8bc1a9359d
shyjuissac <shyjuissacalex@gmail.com>
2021-07-01 18:09:29 +0530

    morecontent

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
+import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import Menu2 from './components/Menu2';
+import MoreContent from './components/MoreContent';
 
 function App() {
   return (
     <div className="App">
-<Header/>
-<Menu2/>
+        <Header/>
+        <Menu2/>
+        <MoreContent/>
     </div>
   );
 }
