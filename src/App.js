6f584c54efdf083f839167ad32ac9f8331b7ed4b
shyjuissac <shyjuissacalex@gmail.com>
2021-07-01 17:36:00 +0530

    menu added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './components/Header';
+import Menu2 from './components/Menu2';
 
 function App() {
   return (
     <div className="App">
 <Header/>
+<Menu2/>
     </div>
   );
 }
